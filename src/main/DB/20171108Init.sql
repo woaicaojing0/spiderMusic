3c6348910e432d94b287cd31c969b2c864df9262
cj34920 <cj34920@ly.com>
2018-06-07 14:13:55 +0800

    初始化DB

--- src/main/DB/20171108Init.sql
+++ src/main/DB/20171108Init.sql
@@ -2,7 +2,7 @@
 SQLyog Community v12.2.4 (64 bit)
 MySQL - 5.7.19 : Database - spiderhouse
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -16,27 +16,42 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`spiderhouse` /*!40100 DEFAULT CHARACTER
 
 USE `spiderhouse`;
 
+/*Table structure for table `day_count` */
+
+DROP TABLE IF EXISTS `day_count`;
+
+CREATE TABLE `day_count` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `day` DATETIME DEFAULT NULL COMMENT '日期',
+  `NewHouseCount` INT(11) DEFAULT NULL COMMENT '新房数量',
+  `SecondHouseCount` INT(11) DEFAULT NULL COMMENT '二手房数量',
+  `TenementCount` INT(11) DEFAULT NULL COMMENT '租房数量',
+  `type` ENUM('DAY','MONTH','YEAR') COLLATE utf8_bin DEFAULT NULL COMMENT '类型有日 月 年',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 /*Table structure for table `new_house_info` */
 
 DROP TABLE IF EXISTS `new_house_info`;
 
 CREATE TABLE `new_house_info` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `houseId` int(11) DEFAULT NULL,
-  `houseName` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `houseAddress` varchar(400) COLLATE utf8_bin DEFAULT NULL,
-  `houseBig` varchar(200) COLLATE utf8_bin DEFAULT NULL,
-  `houseType` varchar(200) COLLATE utf8_bin DEFAULT NULL,
-  `referencePrice` decimal(10,0) DEFAULT NULL,
-  `referencePriceUnit` varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  `referencePriceType` varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  `circumPrice` decimal(10,0) DEFAULT NULL,
-  `contactNumber` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `imgUrl` text COLLATE utf8_bin,
-  `baiduLat` varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  `baiduLng` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `houseId` INT(11) DEFAULT NULL,
+  `houseName` VARCHAR(100) COLLATE utf8_bin DEFAULT NULL,
+  `houseAddress` VARCHAR(400) COLLATE utf8_bin DEFAULT NULL,
+  `houseBig` VARCHAR(200) COLLATE utf8_bin DEFAULT NULL,
+  `houseType` VARCHAR(200) COLLATE utf8_bin DEFAULT NULL,
+  `referencePrice` DECIMAL(10,0) DEFAULT NULL,
+  `referencePriceUnit` VARCHAR(20) COLLATE utf8_bin DEFAULT NULL,
+  `referencePriceType` VARCHAR(20) COLLATE utf8_bin DEFAULT NULL,
+  `circumPrice` DECIMAL(10,0) DEFAULT NULL,
+  `contactNumber` VARCHAR(100) COLLATE utf8_bin DEFAULT NULL,
+  `imgUrl` TEXT COLLATE utf8_bin,
+  `baiduLng` VARCHAR(40) COLLATE utf8_bin DEFAULT NULL,
+  `baiduLat` VARCHAR(40) COLLATE utf8_bin DEFAULT NULL,
+  `createTime` DATE DEFAULT NULL COMMENT '创建日期',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2326 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=INNODB AUTO_INCREMENT=1743 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
